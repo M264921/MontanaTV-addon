3be04b2afe42da2799020fdf407153e5df3adcf9
M264921 <m264921@example.com>
2025-03-26 20:45:27 +0100

    Unificación de addons

--- addon.js
+++ addon.js
@@ -1,24 +1,61 @@
-const { addonBuilder } = require("stremio-addon-sdk");
+const { addonBuilder } = require("stremio-addon-sdk")
 
+// Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
 const manifest = {
-    "id": "org.antonio.montanatv",
-    "version": "1.0.0",
-    "name": "MontanaTV",
-    "description": "Addon personalizado que convierte acestream:// en tu servidor remoto",
-    "resources": ["stream"],
-    "types": ["movie", "tv"],
-    "catalogs": [],
-    "idPrefixes": ["acestream"]
-};
+	"id": "community.MontanaTv",
+	"version": "0.0.1",
+	"catalogs": [
+		{
+			"type": "movie",
+			"id": "top"
+		}
+	],
+	"resources": [
+		"catalog",
+		"stream",
+		"meta"
+	],
+	"types": [
+		"movie",
+		"series",
+		"channel",
+		"tv"
+	],
+	"name": "MontanaTv",
+	"description": "P2P watching"
+}
+const builder = new addonBuilder(manifest)
 
-const builder = new addonBuilder(manifest);
+builder.defineCatalogHandler(({type, id, extra}) => {
+	console.log("request for catalogs: "+type+" "+id)
+	// Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineCatalogHandler.md
+	return Promise.resolve({ metas: [
+		{
+			id: "tt1254207",
+			type: "movie",
+			name: "The Big Buck Bunny",
+			poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg"
+		}
+	] })
+})
 
-builder.defineStreamHandler(({ type, id }) => {
-    const stream = {
-        title: "MontanaTV Stream",
-        url: `http://toninomontana.ddns.net:6878/ace/getstream?id=${id}`
-    };
-    return Promise.resolve({ streams: [stream] });
-});
+builder.defineMetaHandler(({type, id}) => {
+	console.log("request for meta: "+type+" "+id)
+	// Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineMetaHandler.md
+	return Promise.resolve({ meta: null })
+})
 
-module.exports = builder.getInterface();
+builder.defineStreamHandler(({type, id}) => {
+	console.log("request for streams: "+type+" "+id)
+	// Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineStreamHandler.md
+	if (type === "movie" && id === "tt1254207") {
+		// serve one stream to big buck bunny
+		const stream = { url: "http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4" }
+		return Promise.resolve({ streams: [stream] })
+	}
+
+	// otherwise return no streams
+	return Promise.resolve({ streams: [] })
+})
+
+module.exports = builder.getInterface()
